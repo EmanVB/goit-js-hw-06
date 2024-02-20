dc27da5d6ab5ea27708405d58b7b267590f40138
EmanVB <emanbonsato@yahoo.com>
2024-02-20 18:05:11 +0800

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+let colorBtn = document.querySelector(".change-color");
+let htmlBody = document.querySelector("body");
+let colorMsg = document.querySelector(".color");
+
+function setColor() {
+  let colorSet = getRandomHexColor();
+  htmlBody.style.backgroundColor = colorSet;
+  colorMsg.textContent = `${colorSet}`;
+}
+
+colorBtn.addEventListener("click", setColor);
