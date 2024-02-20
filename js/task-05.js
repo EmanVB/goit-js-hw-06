4dd11cad56fef9cb065c4d5b777387301d448a44
EmanVB <emanbonsato@yahoo.com>
2024-02-20 15:41:36 +0800

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,14 @@
 let inputVal = document.getElementById("name-input");
 let outputVal = document.getElementById("name-output");
 
-inputVal.addEventListener ("input", updateValue);
+inputVal.addEventListener("input", updateValue);
+inputVal.addEventListener("focusout", anonymize);
+
+function anonymize() {
+    if (inputVal.value === "") {
+        outputVal.innerText = "Anonymous";
+    }
+}
 
 function updateValue () {
     outputVal.innerText = inputVal.value;
